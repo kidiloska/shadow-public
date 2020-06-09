284f772e9f8ce33f1bf8237363386c71e17e1400
toxic-dev0 <66657797+toxic-dev0@users.noreply.github.com>
2020-06-09 12:44:17 +0530

    Update fun_strings.py

--- shadow/modules/fun_strings.py
+++ shadow/modules/fun_strings.py
@@ -21,144 +21,100 @@ RUN_STRINGS = (
     "As The Doctor would say... RUN!",
 )
 
+DARK_STRINGS = (
+    "പ്ഫാ പന്ന പരനാറി 😠 🤬",
+    "നിന്റെ കുഞ്ഞമ്മേടെ നായർ 😤 🤫",
+    "നീ വല്ല്യ മലരൻ ആണല്ലോടാ..  🤬 🤨",
+    "പ്ഫാ ഇറങ്ങി പോടാ നാറി 🤨",
+    "കടക്കു പുറത്ത് 😕",
+    "ഗ്രൂപ്പിൽ നിന്ന് ഇറങ്ങി പോടാ നാറി 😠 🤬",
+    "പ്ഫാ പന്ന പട്ടി 🥴🥴",
+    "നീ ഏതാടാ ഊളേ 🙄",
+    "പോടാ പട്ടി",
+    "ഡാ പന്നി ",
+    "പുന്നാര മോനെ",
+    "പോടാ മാക്രി",
+    "പോടാ നാറി",
+    "വാടാ ... പട്ടീ",
+    "പോയി ചാവടാ 🤬",
+    "നീ പോടാ കാട്ടുകോഴി 🤨",
+    "പോയി ചത്തൂടെ നിനക്ക്",
+    "കോപ്പേ വല്യ ബഹളം വേണ്ട 😠",
+    "വല്യ മലരനാണല്ലോടാ നീ 🤨",
+    "മണ്ണുണ്ണി",
+    "ഡാ പന്നക്കിളവ",
+    " നിന്റെ കുഞ്ഞമ്മേടെ നായർ",
+    "നിന്റെ അപ്പൂപ്പനോട്‌ പോയി പറ",
+    "പോ മലരേ"
+)
+
 SLAP_SAITAMA_TEMPLATES = (
-    "Slap me one more time and I'll mute you.",
-    "Stop slapping me. REEEEEEEEEEEEEE.",
+    "എന്നെ സ്ലാപ്പ് ചെയ്യാൻ തക്കം നീ വളർന്നോ..?  🥴",
+    "കണ്ട ഊള ബോട്ടുകളെ സ്ലാപ് ചെയുന്ന പോലെ എനിക്കിട്ട് ഉണ്ടാക്കല്ലേ അരിഞ്ഞു കളയും ഞാൻ പന്നി 😏 ",
+    [
+        "പോയിട്ട് പിന്നെ വാ മോനെ 😌",  # normal reply
+        "എന്തിനാ പ്പോ എനിക്കിട്ട് കൊട്ടുന്നെ 😒",  # reply to admin
+        "tmute"  # command
+    ]
+)
+
+
+SLAP_MASTER_TEMPLATES = (
+    "അവൻ എന്റെ മാസ്റ്റർ ആണ് തൊട്ടാൽ കൈ ഞാൻ വെട്ടും 🤬",
+    "ഓട്രാ ചള്ള് ചെക്കാ. ഇമ്മടെ പിള്ളാരെ തൊടാൻ നീ ആയിട്ടില്ല. ‼️",
     [
         "I am muting you for a minute.",  # normal reply
-        "Stop slapping me just because I can't mute you. REEEEEEEEEE.",  # reply to admin
+        "ന്യയോ. 😶",  # reply to admin
+        "tmute"  # command
+    ]
+)
+
+
+SLAP_ABHI_TEMPLATES = (
+    "കൊച്ചിനെ തൊടണേൽ നീ ഒന്നുടെ ജനിക്കണം 👀",
+    "അവളെ തൊടാൻ നീ ആയിട്ടില്ല മോനുസേ പോയിട്ട് പിന്നെ വാ 🤨",
+    "ഇനി അവളെ സ്ലാപ് ചെയ്യാൻ നിന്നാൽ പൊന്നു മോനെ ഔസേപ്പേ നിന്നെ mute ആക്കി വിടും ഞാൻ 🤫🤫 ", 
+    "ടാ ഇമ്മടെ കൊച്ചിനെ സ്ലാപ്പുന്നോ കാലേ വാരി നിലത്തടിക്കും ഞാൻ 🤨🤨", 
+    [
+        "I am muting you for a minute.",  # normal reply
+        "Nyoo.. 😑",  # reply to admin
+        "tmute"  # command
+    ]
+)
+
+
+SLAP_KANNAN_TEMPLATES = (
+    "ഞങ്ങടെ കണ്ണപ്പനെ സ്ലാപ് ചെയ്യാൻ നീ ആരാടാ ഊള ചെക്കാ..?  🤭🤭",
+    "അവന്റെ രോമത്തിൽ തൊടാൻ പറ്റില്ല നിനക്കൊന്നും 🤨🤨",
+    "നീയല്ല നിന്റെ മുത്തച്ഛൻ വിചാരിച്ചാൽ പോലും ചെക്കനെ സ്ലാപ് ചെയ്യാൻ പറ്റില്ല", 
+
+    [
+        "I am muting you for a minute.",  # normal reply
+        "ചെക്കനെ സ്ലാപ്പ് ചെയ്യാൻ സമ്മതിക്കില്ല 😶",  # reply to admin
         "tmute"  # command
     ]
 )
 
 SLAP_TEMPLATES = (
-    "{user2} was shot by {user1}.",
-    "{user2} walked into a cactus while trying to escape {user1}.",
-    "{user2} drowned whilst trying to escape {user1}.",
-    "{user2} fell into a patch of cacti.",
-    "{user2} went up in flames.",
-    "{user2} burned to a fine crisp.",
-    "{user2} was burnt to a crisp whilst fighting {user1}.",
-    "{user2} was struck by lightning.",
-    "{user2} was slain by {user1}.",
-    "{user2} was killed by magic.",
-    "{user2} starved without pats.",
-    "{user2} fell out of the world.",
-    "{user2} was knocked into the void by {user1}.",
-    "{user2}'s bones are scraped clean by the desolate wind.",
-    "{user2} fainted.",
-    "{user2} is out of usable Pokemon! {user2} whited out!.",
-    "{user2} is out of usable Pokemon! {user2} blacked out!.",
-    "{user2} says goodbye to this cruel world.",
-    "{user2} got rekt.",
-    "{user2} was sawn in half by {user1}.",
-    "{user2}'s melon was split by {user1}.",
-    "{user2} was sliced and diced by {user1}.",
-    "{user2} died from {user1}'s mysterious tropical disease.",
-    "{user2} played hot-potato with a grenade.",
-    "{user2} was knifed by {user1}.",
-    "{user2} ate a grenade.",
-    "{user2} is what's for dinner!",
-    "{user2} was terminated by {user1}.",
-    "{user2} was shot before being thrown out of a plane.",
-    "{user2} has encountered an error.",
-    "{user2} died and reincarnated as a goat.",
-    "{user1} threw {user2} off a building.",
-    "{user2} got a premature burial.",
-    "{user1} spammed {user2}'s email.",
-    "{user1} hit {user2} with a small, interstellar spaceship.",
-    "{user1} put {user2} in check-mate.",
-    "{user1} RSA-encrypted {user2} and deleted the private key.",
-    "{user1} put {user2} in the friendzone.",
-    "{user1} slaps {user2} with a DMCA takedown request!",
-    "{user2} died of hospital gangrene.",
-    "{user2} got a house call from Doctor {user1}.",
-    "{user1} beheaded {user2}.",
-    "{user2} got stoned...by an angry mob.",
-    "{user1} sued the pants off {user2}.",
-    "{user2} was one-hit KO'd by {user1}.",
-    "{user1} sent {user2} down the memory hole.",
-    "{user2} was a mistake. - '{user1}' ",
-    "{user1} checkmated {user2} in two moves.",
-    "{user2} was made redundant.",
-    "{user1} {hits} {user2} with a bat!.",
-    "{user1} {hits} {user2} with a Taijutsu Kick!.",
-    "{user1} {hits} {user2} with X-Gloves!.",
-    "{user1} {hits} {user2} with a Jet Punch!.",
-    "{user1} {hits} {user2} with a Jet Pistol!.",
-    "{user1} {hits} {user2} with a United States of Smash!.",
-    "{user1} {hits} {user2} with a Detroit Smash!.",
-    "{user1} {hits} {user2} with a Texas Smash!.",
-    "{user1} {hits} {user2} with a California Smash!.",
-    "{user1} {hits} {user2} with a New Hampshire Smash!.",
-    "{user1} {hits} {user2} with a Missouri Smash!.",
-    "{user1} {hits} {user2} with a Carolina Smash!.",
-    "{user1} {hits} {user2} with a King Kong Gun!.",
-    "{user1} {hits} {user2} with a baseball bat - metal one.!.",
-    "*Serious punches {user2}*.",
-    "*Normal punches {user2}*.",
-    "*Consecutive Normal punches {user2}*.",
-    "*Two Handed Consecutive Normal Punches {user2}*.",
-    "*Ignores {user2} to let them die of embarassment*.",
-    "*points at {user2}* What's with this sassy... lost child?.",
-    "*Hits {user2} with a Fire Tornado*.",
-    "{user1} pokes {user2} in the eye !",
-    "{user1} pokes {user2} on the sides!",
-    "{user1} pokes {user2}!",
-    "{user1} pokes {user2} with a needle!",
-    "{user1} pokes {user2} with a pen!",
-    "{user1} pokes {user2} with a stun gun!",
-    "{user2} is secretly a Furry!.",
-    "Hey Everybody! {user1} is asking me to be mean!",
-    "( ･_･)ﾉ⌒●~* (･.･;) <-{user2}",
-    "Take this {user2}\n(ﾉﾟДﾟ)ﾉ ))))●~* ",
-    "Here {user2} hold this\n(｀・ω・)つ ●~＊",
-    "( ・_・)ノΞ●~*  {user2}\nDieeeee!!.",
-    "( ・∀・)ｒ鹵~<≪巛;ﾟДﾟ)ﾉ\n*Bug sprays {user2}*.",
-    "( ﾟДﾟ)ﾉ占~<巛巛巛.\n-{user2} You pest!",
-    "( う-´)づ︻╦̵̵̿╤── \(˚☐˚”)/ {user2}.",
-    "{user1} {hits} {user2} with a {item}.",
-    "{user1} {hits} {user2} in the face with a {item}.",
-    "{user1} {hits} {user2} around a bit with a {item}.",
-    "{user1} {throws} a {item} at {user2}.",
-    "{user1} grabs a {item} and {throws} it at {user2}'s face.",
-    "{user1} launches a {item} in {user2}'s general direction.",
-    "{user1} starts slapping {user2} silly with a {item}.",
-    "{user1} pins {user2} down and repeatedly {hits} them with a {item}.",
-    "{user1} grabs up a {item} and {hits} {user2} with it.",
-    "{user1} ties {user2} to a chair and {throws} a {item} at them.",
-    "{user1} gave a friendly push to help {user2} learn to swim in lava.",
-    "{user1} bullied {user2}.",
-    "Nyaan ate {user2}'s leg. *nomnomnom*",
-    "{user1} {throws} a master ball at {user2}, resistance is futile.",
-    "{user1} hits {user2} with an action beam...bbbbbb (ง・ω・)ง ====*",
-    "{user1} ara ara's {user2}.",
-    "{user1} ora ora's {user2}.",
-    "{user1} muda muda's {user2}.",
-    "{user2} was turned into a Jojo reference!",
-    "{user1} hits {user2} with {item}.",
-    "Round 2!..Ready? .. FIGHT!!",
-    "WhoPixel will oof {user2}, to infinity and beyond.",
-    "{user2} ate a bat and discovered a new disease.",
-    "{user1} folded {user2} into a paper plane",
-    "{user1} exchanged {user2}'s life-force for a cup of chocolate.",
-    "{user2} did a 69 with a cactus.",
-    "{user1} served {user2} some bat soup.",
-    "{user2} was sent to his home, the planet of apes.",
-    "{user1} kicked {user2} out of a moving train.",
-    "{user1} traveled to the future and spat on {user2}'s grave.",
-    "{user2} died from talk-no-jutsu.",
-    "{user2} was placed as a carpet for a stomp dance competition.",
-    "{user2} just killed John Wick’s dog.",
-    "{user1} performed an Avada Kedavra spell on {user2}.",
-    "{user1} subjected {user2} to a fiery furnace.",
-    "Sakura Haruno just got more useful than {user2}",
-    "{user1} unplugged {user2}'s life support.",
-    "{user1} subscribed {user2}' to 5 years of bad internet.",
-    "{user1} learned why being a crocodile dentist was a bad idea.",
-    "You know what’s worse than Dad jokes? {user2}!",
-    "{user1} took all of {user2}'s cookies.",
-    "{user2} wa mou.......Shindeiru! - {user1}.",
+    "{user1} {user2} ന്റെ ചെവിക്കല് അടിച്ചു പൊളിച്ചു 😐😔",
+"{user1} {user2} ന്റെ മുഖത്തു തിളച്ച എണ്ണ എടുത്ത് ഒഴിച്ചു  😵🥴",
+"{user1} {user2} നെ കാലേ വാരി നിലത്തടിച്ചു 🤓☹️ ",
+"{user1} {user2} ന്റെ ചെവിക്ക് പിടിച്ചു തൂക്കി എറിഞ്ഞു 🥴🥴",
+"{user1} ഓടി വന്നു {user2} നെ ചവിട്ടി നിലത്ത്  ഇട്ട് രണ്ട് ചവിട്ട് കൊടുത്തു 😳😳  ",
+"{user1} {user2} ന്റെ കുണ്ടിക്ക് ചട്ടുകം പഴുപ്പിച്ചു വച്ചു 🤯🤯 ",
+"{user1} {user2} ന്റെ മുഖത്തു ചാണകം കലക്കി ഒഴിച്ചു 💩💩",
+"{user1} ഓടി വന്നു  {user2} ന്റെ ചന്തിക്കു ചൂരല് കൊണ്ട് 5, 6 അടി കൊടുത്തു 😧😧  ",
+"{user1} {user2} നെ എടുത്ത് പൊട്ട കിണറ്റിൽ ഇട്ടു 🤣🤣",
+"{user1} {user2} ന്റെ തലക്കിട്ടു പാറ കല്ലിനു എറിഞ്ഞു 🥺🥺 ",
+"{user1} {user2} നെ കുനിച്ചു നിർത്തി കൂമ്പിനിടിച്ചു 🤭🤣🤣  ",
+"{user1} ഒരു കയർ എടുത്ത് {user2} നെ  മരത്തിൽ കെട്ടി ഇട്ട് ചീ മുട്ട കൊണ്ട് എറിഞ്ഞു 😁😁 ",
+"{user1} {user2} ന്റെ തലക്കിട്ടു അഞ്ചാറു അടി കൊടുത്തു 😪😪 ",
+"{user1} ഓടി വന്നു {user2} നെ ചവിട്ടി  കുളത്തിൽ ഇട്ടു 🤭🤭  ",
+"{user1} {user2} ന്റെ തല അടിച്ചു പൊളിച്ചു 🤥🤥 ",
+"{user1} ഓടി വന്നു {user2} നെ പിടിച്ചു  മോന്ത നോക്കി അഞ്ചാറു അടി കൊടുത്തു 🤪🤪 ",
+"{user1} {user2} നെ  ചവിട്ടി നിലത്തിട്ട് വലിച്ചോണ്ട് പോയി 🤭🤭 ",
+"{user1} ചാണകം വാരി {user2} മോന്തക്ക് എറിഞ്ഞു 🤢🤮 ",
+
 )
 
 PING_STRING = (
