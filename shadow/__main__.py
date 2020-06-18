10a46759e15d6fca4a2b78bbe7b62f20e5387f5a
toxic-dev0 <66657797+toxic-dev0@users.noreply.github.com>
2020-06-18 12:30:48 +0530

    Update __main__.py

--- shadow/__main__.py
+++ shadow/__main__.py
@@ -142,16 +142,14 @@ def start(bot: Bot, update: Update, args: List[str]):
             elif args[0][1:].isdigit() and "rules" in IMPORTED:
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
-            else:
+        else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(KIGYO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add me to your group",
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add Kigyō to your group",
                                                                        url="t.me/{}?startgroup=true".format(bot.username))]]))
-                                                 
-                                                
     else:
-        update.effective_message.reply_text("yeah!, what's up..?)
+        update.effective_message.reply_text("Yo, whadup?")
 
 
 # for test purposes
